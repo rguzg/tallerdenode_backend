b9f68359911d85c337e2c78456f1e9d5ed41b891
Raúl <raulguzmanguerrero@outlook.com>
2020-12-01 09:48:58 -0600

    Even more fixes YAY!

--- utilities/createadmin.js
+++ utilities/createadmin.js
@@ -20,7 +20,7 @@ async function createadmin(username, password, nombre, apellidos) {
     let saltrounds = 10;
     let passwordhash = await bcrypt.hash(password, saltrounds);
 
-    let query = querybuilder("usuarios", { username, passwordHash, nombre, apellidos });
+    let query = querybuilder("usuarios", { username, password: passwordhash, nombre, apellidos });
 
     await db
         .query(query)
