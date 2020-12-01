8811be153045ae343f2be1b8555043ba7db5d871
Raúl <raulguzmanguerrero@outlook.com>
2020-11-30 19:03:21 -0600

    Added /usuario and /empleados routes

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-const app = require("express");
+const express = require("express");
 const dotenv = require("dotenv");
-const endpoints = require("./endpoints.js");
+const endpoints = require("./endpoints");
+
+const app = express();
 
 // Middleware
-const notfound = require("./middleware/notfound.js");
+const notfound = require("./middleware/notfound");
+
+// Routes
+const usuario = require("./routes/usuario");
+const empleados = require("./routes/empleados");
+const auth = require("./middleware/auth");
+
 dotenv.config();
 
+app.use(express.urlencoded({extended: true}));
+
 app.get("/", (req, res, next) => {
-    return res.status(200).json({code: 200, message: endpoints});
+    return res.status(200).json({ code: 200, message: endpoints });
 });
+
+app.use('/usuario', usuario);
+app.use('/empleados', empleados);
 
 app.use(notfound);
 
